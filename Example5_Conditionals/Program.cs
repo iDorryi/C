bb6c3601a23a4b8e075c7cf62310f43f3a0996ad
idorry <iannoyinbox@gmail.com>
2022-09-01 22:13:34 +0300

    Added 5th conditionals case

---
+++ Example5_Conditionals/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "катя")
+{
+    Console.WriteLine("ДА ЭТО ЖЕ КАТЯ!!!");
+}
+else 
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
