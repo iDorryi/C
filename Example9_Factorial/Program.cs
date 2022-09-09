bfbd3218f6d8795679fc703026c05627281700d3
idorry <iannoyinbox@gmail.com>
2022-09-09 22:15:11 +0300

    New

---
+++ Example9_Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine()); // Глобальная , то есть, доступна везде
+Console.WriteLine(GetMultiply(N));
+Console.WriteLine(GetMultiply(5));
+Console.WriteLine(GetMultiply(6));
+Console.WriteLine(GetMultiply(7));
+Console.WriteLine(GetMultiply(8));
+
+// PascalCase - обе буквы заглавные: PrintNumbers
+int GetMultiply (int N) // Вторая переменная, она локальная
+// Она живет только внутри метода, только внутри {}
+{
+    int result = 1; // Число * 0 = 0
+    for (int i = 2; i <= N; i++)
+    {
+        result *= i; // result = result * i
+    }
+    return result; 
+}
