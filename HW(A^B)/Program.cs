bf804cb0ad81d807a722be0ba16d947f733866c5
idorry <iannoyinbox@gmail.com>
2022-09-09 23:30:43 +0300

    Grades

---
+++ HW(A^B)/Program.cs
@@ -0,0 +1,6 @@
+﻿Console. WriteLine("Введите число  A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console. WriteLine("Введите число  B");
+int B = Convert.ToInt32(Console.ReadLine());
+double result = Math.Pow(A, B);
+Console.WriteLine(result);
