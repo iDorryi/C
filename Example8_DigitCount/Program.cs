f49a1927d3077f88ab676498d3ded4fce7c78385
idorry <iannoyinbox@gmail.com>
2022-09-09 21:59:46 +0300

    Added DigitCount

---
+++ Example8_DigitCount/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetCountNumbers(number));
+int GetCountNumbers(int num)
+{   // Переменная может называться как угодно
+    int count = 0; // Изначально нет числа и 0 цифр
+    while (num > 0)
+    {
+        count++;
+        num /= 10;
+    }
+    return count;  // Количество цифр в числе
+}
