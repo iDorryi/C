b875d6f7706fa645fa341fe69c3ba67eb94518f4
idorry <iannoyinbox@gmail.com>
2022-09-09 22:33:21 +0300

    Tabnine Ai

---
+++ Example_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] GetBinaryArray(int size)
+{
+    int[] array = new int[size]; // size = 5, array = [0,0,0,0,0]
+    for (int i = 0; i < array.Length; i++) // array.Length = size
+    {
+        array[i] = new Random().Next(2); // Next(5) => от 0 до 4
+    }
+    return array;
+}
+int[] resultArray = GetBinaryArray(8);
+
+// Конвертируем массив в строчку
+
+Console.WriteLine($"Result array: [ {String.Join("; ", resultArray)} ]");
