c1c994f0a84f760b4b5abab15a892f1ca4c8fe2b
idorry <iannoyinbox@gmail.com>
2022-09-10 01:26:59 +0300

    Done with 4th HW

---
+++ HW_3.Array_Print/Program.cs
@@ -0,0 +1,11 @@
+﻿int [] array = {1, 3, 5, 2, 7, 19, 4};
+void PrintArray(int[] array)
+{
+    int l = array.Length;
+    for (int i = 0; i < l; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
