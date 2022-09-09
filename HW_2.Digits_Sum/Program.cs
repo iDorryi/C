c84e9890369ca1c428bf63c05dbc8a8c7c1db1b0
idorry <iannoyinbox@gmail.com>
2022-09-10 00:37:59 +0300

    Sum_digits

---
+++ HW_2.Digits_Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(Sum(42414));
+ 
+int Sum(int value)
+{
+int result = 0;
+result += value % 10;
+value /= 10;
+if(value > 0)
+ {
+  result += Sum(value);
+ }
+return result;
+}
+ 
+
+
