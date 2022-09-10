2b9ecb49de99fe83af767a54d9c4c77290b170f1
idorry <iannoyinbox@gmail.com>
2022-09-10 22:47:35 +0300

    Cycle

--- HW_1.A^B/Program.cs
+++ HW_1.A^B/Program.cs
@@ -7,18 +7,30 @@
 
 
 
+// Console.Write("Введите число A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// int aGrade = Grade(A, B);
+// Console.WriteLine($"Число {A} в степени {B} = {aGrade}");
+// int Grade(int A, int B)
+// {
+//   int result = 1;
+//   for(int i = 1; i <= B; i++){
+//     result = result * A;
+//   }
+//     return result;
+// }
+
 Console.Write("Введите число A: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int B = Convert.ToInt32(Console.ReadLine());
-
-int aGrade = Grade(A, B);
-Console.WriteLine($"Число {A} в степени {B} = {aGrade}");
-int Grade(int A, int B)
-{
-  int result = 1;
-  for(int i = 1; i <= B; i++){
+int result = 1;
+  for(int i = 1; i <= B; i++)
+  {
     result = result * A;
   }
-    return result;
-}
+Console.WriteLine($"Число {A} в степени {B} = {result}");
+
