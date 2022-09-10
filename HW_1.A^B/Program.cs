97bdd606d3917d39615a095667e58c14c8d48642
idorry <iannoyinbox@gmail.com>
2022-09-10 22:39:51 +0300

    A^B

--- HW_1.A^B/Program.cs
+++ HW_1.A^B/Program.cs
@@ -1,6 +1,24 @@
-﻿Console. WriteLine("Введите число  A");
+﻿// Console. WriteLine("Введите число  A");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console. WriteLine("Введите число  B");
+// int B = Convert.ToInt32(Console.ReadLine());
+// double result = Math.Pow(A, B);
+// Console.WriteLine(result);
+
+
+
+Console.Write("Введите число A: ");
 int A = Convert.ToInt32(Console.ReadLine());
-Console. WriteLine("Введите число  B");
+Console.Write("Введите число B: ");
 int B = Convert.ToInt32(Console.ReadLine());
-double result = Math.Pow(A, B);
-Console.WriteLine(result);
+
+int aGrade = Grade(A, B);
+Console.WriteLine($"Число {A} в степени {B} = {aGrade}");
+int Grade(int A, int B)
+{
+  int result = 1;
+  for(int i = 1; i <= B; i++){
+    result = result * A;
+  }
+    return result;
+}
