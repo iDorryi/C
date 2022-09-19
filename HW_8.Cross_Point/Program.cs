db55205723181cd8a67fac38b280d7a19797f22a
idorry <iannoyinbox@gmail.com>
2022-09-20 01:20:40 +0300

    Doubled

--- HW_8.Cross_Point/Program.cs
+++ HW_8.Cross_Point/Program.cs
@@ -1,16 +1,17 @@
 ﻿Console.WriteLine("Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем:");
 Console.Write("Введите k1: ");
-int k1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите b1: ");
-int b1 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите k2: ");
-int k2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите b2: ");
-int b2 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
 
-double xres = (b2-b1)/(k1-k2);
-double yres = k1 * (b2-b1)/(k1-k2) + b1;
-Console.WriteLine("\n" +$"Прямые пересекаются в точке с координатами:" + $"x = {xres}; " + $"y= {yres}");
+double x = (b2-b1)/(k1-k2);
+double y = k1 * (b2-b1)/(k1-k2) + b1;
+Console.WriteLine("\n" +$"Прямые пересекаются в точке с координатами:" + $"x = {x}; " + $"y= {y}");
+
