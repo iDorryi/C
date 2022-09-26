5b269eedbb539a3f172d3d62618a7ab7950de78a
idorry <iannoyinbox@gmail.com>
2022-09-27 01:08:58 +0300

    Created double matrix

---
+++ HW_9.Random_Matrix/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Задайте двумерный массив размером mxn, заполненный случайными вещественными числами:");
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] GetMatrix (int m, int n)
+{
+  double[,] matrix = new double[m, n];
+  for (int i = 0; i < matrix.GetLength(0); i++)  
+  {
+    for ( int j = 0; j < n; j++ )
+    {
+        matrix[i,j] = new Random().NextDouble() * 10;
+    }
+  }
+  return matrix;
+}
+void PrintMatrix(double[,] inputmatrix)
+{
+    for (int i = 0; i < inputmatrix.GetLength(0); i++)
+    {
+       for (int m = 0; m < inputmatrix.GetLength(1); m++)
+       {
+        Console.Write(inputmatrix[i,m] + "\t");
+       }
+       Console.WriteLine();
+    }
+    
+}
+double [,] resultMatrix = GetMatrix(rows, columns);
+PrintMatrix(resultMatrix);
