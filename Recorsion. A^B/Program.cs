f8e7c42f43010b5b822dbb96caf17cfefb3a0930
idorry <iannoyinbox@gmail.com>
2022-10-01 22:45:52 +0300

    Power

---
+++ Recorsion. A^B/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int degree = Convert.ToInt32(Console.ReadLine());
+int PowNumber (int number, int power)
+{
+    if(power == 0) return 1;
+    if (power == 1 ) return number;
+    return (number * PowNumber(number, power - 1 ));
+}
+System.Console.WriteLine(PowNumber(number, degree));
