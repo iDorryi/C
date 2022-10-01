181cffa5885d84cb9b34a69139b6f163d2dbb668
idorry <iannoyinbox@gmail.com>
2022-10-01 15:56:02 +0300

    another verrsion

--- Recorsion.Numbers_Untill_N/Program.cs
+++ Recorsion.Numbers_Untill_N/Program.cs
@@ -1,9 +1,21 @@
-﻿Console.WriteLine("Введите N");
+﻿// Console.WriteLine("Введите N");
+// int N = Convert.ToInt32(Console.ReadLine());
+// string NumbersUntillN ( int start, int end)
+// {
+//     if(start == end)
+//     return start.ToString();
+//     return (start + "," + NumbersUntillN(start + 1, end ));
+// }
+// Console.WriteLine($"Номера до N это: {NumbersUntillN (1, N)}");
+
+Console.WriteLine("Введите M");
+Console.WriteLine("Введите N");
 int N = Convert.ToInt32(Console.ReadLine());
-string NumbersUntillN ( int start, int end)
+int M = Convert.ToInt32(Console.ReadLine());
+string NumbersUntillN ( int M, int N)
 {
-    if(start == end)
-    return start.ToString();
-    return (start + "," + NumbersUntillN(start + 1, end ));
+    if(M == N)
+    return M.ToString();
+    return (M + "," + NumbersUntillN(M + 1, N ));
 }
-Console.WriteLine($"Номера до N это: {NumbersUntillN (1, N)}");
+Console.WriteLine($"Номера от M до N это: {NumbersUntillN(M, N)}");
