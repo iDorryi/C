38e5f8c165064a967d01f266bdf22c37d8334499
idorry <iannoyinbox@gmail.com>
2022-10-01 22:23:18 +0300

    SumOfDigits connects

---
+++ Recorsion. SumOfDigits/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+int num = Convert.ToInt32(Console.ReadLine());
+int SumDigits (int num)
+{
+    if(num % 10 == 0) return 0;
+    return (num % 10 + SumDigits(num/10));
+}
+System.Console.WriteLine(SumDigits(num));
