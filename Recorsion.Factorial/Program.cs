c952c8759681d7ee7eb8148ba49086a700db7812
idorry <iannoyinbox@gmail.com>
2022-10-01 15:40:22 +0300

    Added another recorsion example

--- Recorsion.Factorial/Program.cs
+++ Recorsion.Factorial/Program.cs
@@ -1,7 +1,7 @@
 ﻿int Factorial(int N)
 {
     if (N == 1) 
-    return 1;
+    return 1; // Факториал от 1 это 1;
     return (N * Factorial (N-1));
 }
 Console.WriteLine(Factorial(3));
---
+++ Recorsion.Numbers_Untill_N/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите N");
+int N = Convert.ToInt32(Console.ReadLine());
+string NumbersUntillN ( int start, int end)
+{
+    if(start == end)
+    return start.ToString();
+    return (start + "," + NumbersUntillN(start + 1, end ));
+}
+Console.WriteLine($"Номера до N это: {NumbersUntillN (1, N)}");
