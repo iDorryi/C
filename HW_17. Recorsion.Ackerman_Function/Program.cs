7730a948e60110bf2904fd3127b80f04569e33a6
idorry <iannoyinbox@gmail.com>
2022-10-02 22:08:30 +0300

    Ackerman

---
+++ HW_17. Recorsion.Ackerman_Function/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n: ");
+
+int Input(string message)
+{
+    Console.Write(message);
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+int m = Input("Введите число m: "); 
+int n = Input("Введите число n: ");
+
+    int AckermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if ((m != 0) && (n == 0))
+        {
+            return AckermanFunction(m - 1, 1);
+        }
+        else
+        {
+            return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+        }
+    }
+}
+Console.WriteLine($"Функция Аккермана : {AckermanFunction( m, n)}");
+
